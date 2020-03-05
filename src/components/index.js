4947017bb025c68e9322299678c2323511d53f5c
liulm <luming.liu@dragonsoftbravo.com>
2020-03-05 16:44:27 +0800

    modify

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,17 @@
 import MouseParallaxPlugin from '../plugins/MouseParallax';
 import MouseParallax from './MouseParallax';
 
+MouseParallaxPlugin.setup();
+
 const Parallax = {
   install(Vue) {
     if (typeof window !== 'undefined' && window.Vue) {
       Vue = window.Vue
     }
-    Vue.use(MouseParallaxPlugin);
     Vue.component(MouseParallax.name, MouseParallax);
   },
 };
+
+export { MouseParallax };
 
 export default Parallax;
--- src/plugins/MouseParallax.js
+++ src/plugins/MouseParallax.js
@@ -1,12 +1,9 @@
 import MouseParallax from '../utils/MouseParallax';
 
 const Parallax = {
-  install(Vue) {
-    Object.defineProperty(Vue.prototype, '$MouseParallax', { value: MouseParallax });
-    if (window) {
-      window.MouseParallax = MouseParallax;
-    }
-  },
-};
+  setup() {
+    window.MouseParallax = MouseParallax;
+  }
+}
 
 export default Parallax;
